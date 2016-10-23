3980123bae53034d12d1a1c7c6e284f1e9b65037
diederichk <kyle-diederich@uiowa.edu>
2016-10-23 15:41:22 -0500

    Created Object Grid
    Created grid of selectable buttons

--- AnythingIsFine/source/WpfSamples/ActivatableElements/MainWindowModel.cs
+++ AnythingIsFine/source/WpfSamples/ActivatableElements/MainWindowModel.cs
@@ -9,6 +9,7 @@ namespace ActivatableElements
     using System.ComponentModel;
     using System.Windows.Data;
     using System.Windows.Media;
+    using System;
 
     public class MainWindowModel
     {
@@ -35,6 +36,7 @@ namespace ActivatableElements
         /// <param name="activatedNote"></param>
         public void ActivateNote(Note activatedNote)
         {
+            Console.Write("Activated note:" + activatedNote.Text);
             _notes.Remove(activatedNote);
         }
 
@@ -55,18 +57,117 @@ namespace ActivatableElements
         }
 
         private void InitializeNotes(ObservableCollection<Note> notes)
-        {
+        {   // Possible Ranges (0-8),(0-10)
             var id = 0;
-            notes.Add(new Note(id++, 0, 0, Colors.Aqua));
-            notes.Add(new Note(id++, 1, 0, Colors.Aquamarine));
-            notes.Add(new Note(id++, 2, 0, Colors.LightPink));
-            notes.Add(new Note(id++, 3, 0, Colors.LightGoldenrodYellow));
-            notes.Add(new Note(id++, 4, 0, Colors.Aqua));
-            notes.Add(new Note(id++, 5, 0, Colors.Aquamarine));
-            notes.Add(new Note(id++, 6, 0, Colors.LightPink));
-            notes.Add(new Note(id++, 7, 0, Colors.LightGoldenrodYellow));
-            notes.Add(new Note(id++, 8, 0, Colors.Aqua));
-            notes.Add(new Note(id++, 8, 10, Colors.LightPink));
+            notes.Add(new Note(id++, 0, 0, Colors.Aqua));                   // 1
+            notes.Add(new Note(id++, 1, 0, Colors.Aquamarine));             // 2
+            notes.Add(new Note(id++, 2, 0, Colors.LightPink));              // 3
+            notes.Add(new Note(id++, 3, 0, Colors.LightGoldenrodYellow));   // 4
+            notes.Add(new Note(id++, 4, 0, Colors.Aqua));                   // 5
+            notes.Add(new Note(id++, 5, 0, Colors.Aquamarine));             // 6
+            notes.Add(new Note(id++, 6, 0, Colors.LightPink));              // 7
+            notes.Add(new Note(id++, 7, 0, Colors.LightGoldenrodYellow));   // 8
+            notes.Add(new Note(id++, 8, 0, Colors.Aqua));                   // 9
+
+            notes.Add(new Note(id++, 0, 1, Colors.Aqua));                   // 1
+            notes.Add(new Note(id++, 1, 1, Colors.Aquamarine));             // 2
+            notes.Add(new Note(id++, 2, 1, Colors.LightPink));              // 3
+            notes.Add(new Note(id++, 3, 1, Colors.LightGoldenrodYellow));   // 4
+            notes.Add(new Note(id++, 4, 1, Colors.Aqua));                   // 5
+            notes.Add(new Note(id++, 5, 1, Colors.Aquamarine));             // 6
+            notes.Add(new Note(id++, 6, 1, Colors.LightPink));              // 7
+            notes.Add(new Note(id++, 7, 1, Colors.LightGoldenrodYellow));   // 8
+            notes.Add(new Note(id++, 8, 1, Colors.Aqua));                   // 9
+
+            notes.Add(new Note(id++, 0, 2, Colors.Aqua));                   // 1
+            notes.Add(new Note(id++, 1, 2, Colors.Aquamarine));             // 2
+            notes.Add(new Note(id++, 2, 2, Colors.LightPink));              // 3
+            notes.Add(new Note(id++, 3, 2, Colors.LightGoldenrodYellow));   // 4
+            notes.Add(new Note(id++, 4, 2, Colors.Aqua));                   // 5
+            notes.Add(new Note(id++, 5, 2, Colors.Aquamarine));             // 6
+            notes.Add(new Note(id++, 6, 2, Colors.LightPink));              // 7
+            notes.Add(new Note(id++, 7, 2, Colors.LightGoldenrodYellow));   // 8
+            notes.Add(new Note(id++, 8, 2, Colors.Aqua));                   // 9
+
+            notes.Add(new Note(id++, 0, 3, Colors.Aqua));                   // 1
+            notes.Add(new Note(id++, 1, 3, Colors.Aquamarine));             // 2
+            notes.Add(new Note(id++, 2, 3, Colors.LightPink));              // 3
+            notes.Add(new Note(id++, 3, 3, Colors.LightGoldenrodYellow));   // 4
+            notes.Add(new Note(id++, 4, 3, Colors.Aqua));                   // 5
+            notes.Add(new Note(id++, 5, 3, Colors.Aquamarine));             // 6
+            notes.Add(new Note(id++, 6, 3, Colors.LightPink));              // 7
+            notes.Add(new Note(id++, 7, 3, Colors.LightGoldenrodYellow));   // 8
+            notes.Add(new Note(id++, 8, 3, Colors.Aqua));                   // 9
+
+            notes.Add(new Note(id++, 0, 4, Colors.Aqua));                   // 1
+            notes.Add(new Note(id++, 1, 4, Colors.Aquamarine));             // 2
+            notes.Add(new Note(id++, 2, 4, Colors.LightPink));              // 3
+            notes.Add(new Note(id++, 3, 4, Colors.LightGoldenrodYellow));   // 4
+            notes.Add(new Note(id++, 4, 4, Colors.Aqua));                   // 5
+            notes.Add(new Note(id++, 5, 4, Colors.Aquamarine));             // 6
+            notes.Add(new Note(id++, 6, 4, Colors.LightPink));              // 7
+            notes.Add(new Note(id++, 7, 4, Colors.LightGoldenrodYellow));   // 8
+            notes.Add(new Note(id++, 8, 4, Colors.Aqua));                   // 9
+
+            notes.Add(new Note(id++, 0, 5, Colors.Aqua));                   // 1
+            notes.Add(new Note(id++, 1, 5, Colors.Aquamarine));             // 2
+            notes.Add(new Note(id++, 2, 5, Colors.LightPink));              // 3
+            notes.Add(new Note(id++, 3, 5, Colors.LightGoldenrodYellow));   // 4
+            notes.Add(new Note(id++, 4, 5, Colors.Aqua));                   // 5
+            notes.Add(new Note(id++, 5, 5, Colors.Aquamarine));             // 6
+            notes.Add(new Note(id++, 6, 5, Colors.LightPink));              // 7
+            notes.Add(new Note(id++, 7, 5, Colors.LightGoldenrodYellow));   // 8
+            notes.Add(new Note(id++, 8, 5, Colors.Aqua));                   // 9
+
+            notes.Add(new Note(id++, 0, 6, Colors.Aqua));                   // 1
+            notes.Add(new Note(id++, 1, 6, Colors.Aquamarine));             // 2
+            notes.Add(new Note(id++, 2, 6, Colors.LightPink));              // 3
+            notes.Add(new Note(id++, 3, 6, Colors.LightGoldenrodYellow));   // 4
+            notes.Add(new Note(id++, 4, 6, Colors.Aqua));                   // 5
+            notes.Add(new Note(id++, 5, 6, Colors.Aquamarine));             // 6
+            notes.Add(new Note(id++, 6, 6, Colors.LightPink));              // 7
+            notes.Add(new Note(id++, 7, 6, Colors.LightGoldenrodYellow));   // 8
+            notes.Add(new Note(id++, 8, 6, Colors.Aqua));                   // 9
+
+            notes.Add(new Note(id++, 0, 7, Colors.Aqua));                   // 1
+            notes.Add(new Note(id++, 1, 7, Colors.Aquamarine));             // 2
+            notes.Add(new Note(id++, 2, 7, Colors.LightPink));              // 3
+            notes.Add(new Note(id++, 3, 7, Colors.LightGoldenrodYellow));   // 4
+            notes.Add(new Note(id++, 4, 7, Colors.Aqua));                   // 5
+            notes.Add(new Note(id++, 5, 7, Colors.Aquamarine));             // 6
+            notes.Add(new Note(id++, 6, 7, Colors.LightPink));              // 7
+            notes.Add(new Note(id++, 7, 7, Colors.LightGoldenrodYellow));   // 8
+            notes.Add(new Note(id++, 8, 7, Colors.Aqua));                   // 9
+
+            notes.Add(new Note(id++, 0, 8, Colors.Aqua));                   // 1
+            notes.Add(new Note(id++, 1, 8, Colors.Aquamarine));             // 2
+            notes.Add(new Note(id++, 2, 8, Colors.LightPink));              // 3
+            notes.Add(new Note(id++, 3, 8, Colors.LightGoldenrodYellow));   // 4
+            notes.Add(new Note(id++, 4, 8, Colors.Aqua));                   // 5
+            notes.Add(new Note(id++, 5, 8, Colors.Aquamarine));             // 6
+            notes.Add(new Note(id++, 6, 8, Colors.LightPink));              // 7
+            notes.Add(new Note(id++, 7, 8, Colors.LightGoldenrodYellow));   // 8
+            notes.Add(new Note(id++, 8, 8, Colors.Aqua));                   // 9
+
+            notes.Add(new Note(id++, 0, 9, Colors.Aqua));                   // 1
+            notes.Add(new Note(id++, 1, 9, Colors.Aquamarine));             // 2
+            notes.Add(new Note(id++, 2, 9, Colors.LightPink));              // 3
+            notes.Add(new Note(id++, 3, 9, Colors.LightGoldenrodYellow));   // 4
+            notes.Add(new Note(id++, 4, 9, Colors.Aqua));                   // 5
+            notes.Add(new Note(id++, 5, 9, Colors.Aquamarine));             // 6
+            notes.Add(new Note(id++, 6, 9, Colors.LightPink));              // 7
+            notes.Add(new Note(id++, 7, 9, Colors.LightGoldenrodYellow));   // 8
+            notes.Add(new Note(id++, 8, 9, Colors.Aqua));                   // 9
+
+            notes.Add(new Note(id++, 0, 10, Colors.Aqua));                   // 1
+            notes.Add(new Note(id++, 1, 10, Colors.Aquamarine));             // 2
+            notes.Add(new Note(id++, 2, 10, Colors.LightPink));              // 3
+            notes.Add(new Note(id++, 3, 10, Colors.LightGoldenrodYellow));   // 4
+            notes.Add(new Note(id++, 4, 10, Colors.Aqua));                   // 5
+            notes.Add(new Note(id++, 5, 10, Colors.Aquamarine));             // 6
+            notes.Add(new Note(id++, 6, 10, Colors.LightPink));              // 7
+            notes.Add(new Note(id++, 7, 10, Colors.LightGoldenrodYellow));   // 8
+            notes.Add(new Note(id++, 8, 10, Colors.Aqua));                   // 9
         }
     }
 
@@ -77,6 +178,7 @@ namespace ActivatableElements
     {
         private readonly List<string> quotes = new List<string>()
             {
+            // 1
                 "1",
                 "2",
                 "3",
@@ -86,8 +188,106 @@ namespace ActivatableElements
                 "7",
                 "8",
                 "9",
-                "10",
-                "11",
+                // 2
+                "1",
+                "2",
+                "3",
+                "4",
+                "5",
+                "6",
+                "7",
+                "8",
+                "9",
+                // 3
+                "1",
+                "2",
+                "3",
+                "4",
+                "5",
+                "6",
+                "7",
+                "8",
+                "9",
+                // 4
+                "1",
+                "2",
+                "3",
+                "4",
+                "5",
+                "6",
+                "7",
+                "8",
+                "9",
+                // 5
+                "1",
+                "2",
+                "3",
+                "4",
+                "5",
+                "6",
+                "7",
+                "8",
+                "9",
+                // 6
+                "1",
+                "2",
+                "3",
+                "4",
+                "5",
+                "6",
+                "7",
+                "8",
+                "9",
+                // 7
+                "1",
+                "2",
+                "3",
+                "4",
+                "5",
+                "6",
+                "7",
+                "8",
+                "9",
+                // 8
+                "1",
+                "2",
+                "3",
+                "4",
+                "5",
+                "6",
+                "7",
+                "8",
+                "9",
+                // 9
+                "1",
+                "2",
+                "3",
+                "4",
+                "5",
+                "6",
+                "7",
+                "8",
+                "9",
+                // 10
+                "1",
+                "2",
+                "3",
+                "4",
+                "5",
+                "6",
+                "7",
+                "8",
+                "9",
+                // 11
+                "1",
+                "2",
+                "3",
+                "4",
+                "5",
+                "6",
+                "7",
+                "8",
+                "9",
             }; 
 
         public Note(int id, int columnIndex, int rowIndex, Color backgroundColor)

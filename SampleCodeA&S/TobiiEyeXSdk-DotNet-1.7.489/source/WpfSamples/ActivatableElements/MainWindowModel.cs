f4a71f0c4dca29dda606e0f08129afc01d9436c0
Ziqi Su <ziqsu43@gmail.com>
2016-10-27 16:27:42 -0500

    Three sentence box, it will change color after click it

--- SampleCodeA&S/TobiiEyeXSdk-DotNet-1.7.489/source/WpfSamples/ActivatableElements/MainWindowModel.cs
+++ SampleCodeA&S/TobiiEyeXSdk-DotNet-1.7.489/source/WpfSamples/ActivatableElements/MainWindowModel.cs
@@ -36,6 +36,10 @@ namespace ActivatableElements
         public void ActivateNote(Note activatedNote)
         {
             _notes.Remove(activatedNote);
+            _notes.Add(new Note(activatedNote.Id, activatedNote.ColumnIndex, activatedNote.RowIndex, Colors.Lavender));
+
+     
+
         }
 
         /// <summary>
@@ -57,16 +61,16 @@ namespace ActivatableElements
         private void InitializeNotes(ObservableCollection<Note> notes)
         {
             var id = 0;
+            notes.Add(new Note(id++, 1, 1, Colors.Aqua));
+            notes.Add(new Note(id++, 1, 4, Colors.Aqua));
+            notes.Add(new Note(id++, 1, 7, Colors.Aqua));
+            /*notes.Add(new Note(id++, 2, 1, Colors.LightGoldenrodYellow));
             notes.Add(new Note(id++, 2, 2, Colors.Aqua));
-            notes.Add(new Note(id++, 3, 3, Colors.Aquamarine));
-            notes.Add(new Note(id++, 4, 1, Colors.LightPink));
-            notes.Add(new Note(id++, 4, 5, Colors.LightGoldenrodYellow));
-            notes.Add(new Note(id++, 5, 4, Colors.Aqua));
-            notes.Add(new Note(id++, 3, 7, Colors.Aquamarine));
-            notes.Add(new Note(id++, 6, 6, Colors.LightPink));
-            notes.Add(new Note(id++, 6, 2, Colors.LightGoldenrodYellow));
-            notes.Add(new Note(id++, 2, 6, Colors.Aqua));
-            notes.Add(new Note(id++, 1, 6, Colors.LightPink));
+            notes.Add(new Note(id++, 2, 3, Colors.Aquamarine));
+            notes.Add(new Note(id++, 3, 1, Colors.LightPink));
+            notes.Add(new Note(id++, 3, 2, Colors.LightGoldenrodYellow));
+            notes.Add(new Note(id++, 3, 3, Colors.Aqua));
+            notes.Add(new Note(id++, 4, 1, Colors.LightPink));*/
         }
     }
 
@@ -77,18 +81,20 @@ namespace ActivatableElements
     {
         private readonly List<string> quotes = new List<string>()
             {
+                
                 "You miss 100 percent of the shots you never take.\n—Wayne Gretzky",
                 "I always wanted to be somebody, but now I realize I should have been more specific.\n—Lily Tomlin",
                 "To the man who only has a hammer, everything he encounters begins to look like a nail.\n—Abraham Maslow",
-                "I am extraordinarily patient, provided I get my own way in the end.\n—Margaret Thatcher",
+                /*"I am extraordinarily patient, provided I get my own way in the end.\n—Margaret Thatcher",
                 "Those who believe in telekinetics, raise my hand.\n—Kurt Vonnegut",
                 "It is more important to know where you are going than to get there quickly.\n—Mabel Newcomber",
                 "If you eat a frog first thing in the morning, the rest of your day will be wonderful.\n—Mark Twain",
                 "Age is not important unless you're a cheese.\n—Helen Hayes",
                 "If you have to eat a frog, don’t look at it for too long.\n—Mark Twain",
                 "A thing is mighty big when time and distance cannot shrink it.\n—Zora Neale Hurston",
+                */
             }; 
-
+         
         public Note(int id, int columnIndex, int rowIndex, Color backgroundColor)
         {
             Id = id;
@@ -103,5 +109,15 @@ namespace ActivatableElements
         public int RowIndex { get; set; }
         public string Text { get; set; }
         public SolidColorBrush BackgroundColor { get; set; }
+        
+       /* public SolidColorBrush changeColor(Color color)
+        {
+            this.BackgroundColor = new SolidColorBrush(color);
+            return this.BackgroundColor;
+        }*/
+
+        
+
+        
     }
 }
